27e5a2dbf775cd5c5d59064297df0a360f109e72
Mehmet Yalcinkaya <mehmet.yalcinkaya@aalto.fi>
2021-04-20 22:25:58 +0300

    Tune the params for model performance

--- model.py
+++ model.py
@@ -58,13 +58,13 @@ output_shape = y[0].shape
 tf.random.set_seed(42)
 
 # Create a model using the Sequential API
-model = tf.keras.Sequential([tf.keras.layers.Dense(1), tf.keras.layers.Dense(1)])
+model = tf.keras.Sequential([tf.keras.layers.Dense(1), tf.keras.layers.Dense(2)])
 
 # Compile the model
 model.compile(loss = tf.keras.losses.mae, optimizer = tf.keras.optimizers.SGD(), metrics = ['mae'])
 
 # Fit the model
-model.fit(X_train, y_train, epochs=100)
+model.fit(X_train, y_train, epochs=50)
 
 # Make and plot predictions for model_1
 y_preds = model.predict(X_test)
